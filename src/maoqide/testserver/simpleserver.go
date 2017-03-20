001bedd15aa9f6e489c44c99cb25f592aea47feb
maoqide <maoqiyx@163.com>
2017-03-20 21:00:34 +0800

    simpleserver

--- src/maoqide/testserver/simpleserver.go
+++ src/maoqide/testserver/simpleserver.go
@@ -26,7 +26,8 @@ func (sr SimpleResource) ConvertData(data interface{}) (content []byte, err erro
 func (sr SimpleResource) Get(request *http.Request) (int, interface{}) {
 
 	fmt.Println("HTTP METHOD GET")
-	return http.StatusOK, request.Body
+	res := request.Form
+	return http.StatusOK, res
 }
 
 func (sr SimpleResource) Post(request *http.Request) (int, interface{}) {
@@ -38,7 +39,7 @@ func (sr SimpleResource) Post(request *http.Request) (int, interface{}) {
 	res := map[string]interface{}{}
 	err := json.Unmarshal(buf[:i], &res)
 	if err != nil {
-		fmt.Println("err")
+		fmt.Println(err.Error())
 	}
 	//res := buf[:i]
 	//fmt.Println(string(res))
@@ -48,12 +49,28 @@ func (sr SimpleResource) Post(request *http.Request) (int, interface{}) {
 
 func (sr SimpleResource) Put(request *http.Request) (int, interface{}) {
 
+	buf := make([]byte, 1024)
 	fmt.Println("HTTP METHOD PUT")
-	return http.StatusOK, request.Body
+	i, _ := request.Body.Read(buf)
+
+	res := map[string]interface{}{}
+	err := json.Unmarshal(buf[:i], &res)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return http.StatusOK, res
 }
 
 func (sr SimpleResource) Delete(request *http.Request) (int, interface{}) {
 
+	buf := make([]byte, 1024)
 	fmt.Println("HTTP METHOD DELETE")
-	return http.StatusOK, request.Body
+	i, _ := request.Body.Read(buf)
+
+	res := map[string]interface{}{}
+	err := json.Unmarshal(buf[:i], &res)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return http.StatusOK, res
 }
